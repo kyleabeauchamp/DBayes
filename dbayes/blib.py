5688845cd5df1c332857e9c995c6e23e532b0548
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-09-25 18:50:23 -0400

    Some updates

--- dbayes/blib.py
+++ dbayes/blib.py
@@ -41,10 +41,7 @@ def set_parms(f, sigma, epsilon, q=0.0):
 
 def build(traj, mmtop, temperature, pressure, sigma, epsilon, stderr_tolerance=0.05):
     system = ff.createSystem(mmtop, nonbondedMethod=app.CutoffPeriodic, nonbondedCutoff=1.0 * u.nanometer)
-    f = system.getForce(1)
-    q0, sigma0, epsilon0 = f.getParticleParameters(0)
-    sigma.value = sigma0 / u.nanometers
-    epsilon.value = epsilon0 / u.kilojoule_per_mole
+    f = system.getForce(0)
     set_parms(f, sigma.value, epsilon.value)
 
     measurement = measurements[0]
--- dbayes/test_pymc.py
+++ dbayes/test_pymc.py
@@ -11,6 +11,8 @@ mass = 12.01078 * u.daltons + 4 * 35.4532 * u.daltons
 
 sigma = pm.Uniform("sigma", 0.05, 1.0)
 epsilon = pm.Uniform("sigma", 0.0, 100.0)
+sigma.value = 0.4
+epsilon.value = 3.0
 
 measurements = [dict(temperature=298.15 * u.kelvin, pressure=101.325 * u.kilopascals, density=1584.36 * u.kilograms / (u.meter ** 3.))]
 #measurements.append(dict(temperature=313.15 * u.kelvin, pressure=101.325 * u.kilopascals, density=1554.96 * u.kilograms / (u.meter ** 3.)))
@@ -19,4 +21,4 @@ ff = app.ForceField("./test.xml")
 
 traj, mmtop = blib.build_top()
 
-x, system = blib.build(traj, mmtop, measurements[0]["temperature"], measurements[0]["pressure"], sigma, epsilon)
+system, x = blib.build(traj, mmtop, measurements[0]["temperature"], measurements[0]["pressure"], sigma, epsilon)
