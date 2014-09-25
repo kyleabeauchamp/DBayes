a0b5843645caaaa78657f58bef86401d668910de
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-09-25 19:12:07 -0400

    Some updates

--- dbayes/blib.py
+++ dbayes/blib.py
@@ -1,3 +1,4 @@
+import sys
 import pymbar
 import pymc as pm
 import numpy as np
@@ -61,7 +62,7 @@ def build(traj, mmtop, temperature, pressure, sigma, epsilon, stderr_tolerance=0
 
     simulation = app.Simulation(mmtop, system, integrator)
 
-
+    simulation.reporters.append(app.StateDataReporter(sys.stdout, 2500, density=True))
     simulation.reporters.append(app.StateDataReporter(csv_filename, output_frequency, density=True))
     simulation.context.setPositions(traj.openmm_positions(0))
 
--- dbayes/test_pymc.py
+++ dbayes/test_pymc.py
@@ -11,8 +11,8 @@ mass = 12.01078 * u.daltons + 4 * 35.4532 * u.daltons
 
 sigma = pm.Uniform("sigma", 0.05, 1.0)
 epsilon = pm.Uniform("sigma", 0.0, 100.0)
-sigma.value = 0.4
-epsilon.value = 3.0
+sigma.value = 0.5
+epsilon.value = 8.0
 
 measurements = [dict(temperature=298.15 * u.kelvin, pressure=101.325 * u.kilopascals, density=1584.36 * u.kilograms / (u.meter ** 3.))]
 #measurements.append(dict(temperature=313.15 * u.kelvin, pressure=101.325 * u.kilopascals, density=1554.96 * u.kilograms / (u.meter ** 3.)))
