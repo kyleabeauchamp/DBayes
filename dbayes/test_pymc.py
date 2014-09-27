a4297256f542127afa763e2ed06ed7ccb8d7ddea
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-09-27 12:42:00 -0400

    Checkin 1 simulation version

--- dbayes/test_pymc.py
+++ dbayes/test_pymc.py
@@ -16,9 +16,9 @@ epsilon = pymc.Uniform("epsilon", 10.0, 20.0, value=13.0)
 temperature = 298.15 * u.kelvin
 pressure = 101.325 * u.kilopascals
 
-observed = 1584.36 * u.kilograms / (u.meter ** 3.)
+observed = 1.58436 * u.grams / u.milliliter
 observed = observed / (u.grams / u.milliliter)
-error = 10.0
+error = 0.02
 
 atoms_per_dim = 7
 
@@ -26,7 +26,7 @@ traj, mmtop = blib.build_top(atoms_per_dim, sigma.value)
 
 
 def calc_density(sigma, epsilon):
-    simulation, system, x = blib.build(traj, mmtop, temperature, pressure, sigma, epsilon, nonbondedCutoff=1.2*u.nanometer)
+    simulation, system, x = blib.build(traj, mmtop, temperature, pressure, sigma, epsilon, nonbondedCutoff=1.2*u.nanometer, stderr_tolerance=error)
     t, g, N_eff = pymbar.timeseries.detectEquilibration_fft(x)
     mu = x[t:].mean()
     print("\nmu = %f, target = %f, z = %f" % (mu, observed, (mu - observed) / error))
@@ -38,9 +38,13 @@ measurement = pymc.Normal("observed_density", mu=density, tau=error ** -2., valu
 
 variables = [density, measurement, sigma, epsilon]
 model = pymc.Model(variables)
-mcmc = pymc.MCMC(model)
+mcmc = pymc.MCMC(model, db='hdf5', dbname="./out.h5")
 
 mcmc.sample(10000)
 s = mcmc.trace("sigma")[:]
 e = mcmc.trace("epsilon")[:]
 
+import pymc
+db = pymc.database.hdf5.load("./out.h5")
+s = db.trace("sigma")[:]
+e = db.trace("epsilon")[:]
