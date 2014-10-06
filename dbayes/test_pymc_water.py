fad1c93a5acb80ab4554facf13defa3dddfd019c
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-10-06 14:25:03 -0400

    playing in water

---
+++ dbayes/test_pymc_water.py
@@ -0,0 +1,54 @@
+import pymbar
+import water_lib
+import pymc
+import numpy as np
+import pandas as pd
+import simtk.openmm.app as app
+import simtk.openmm as mm
+import simtk.unit as u
+import mdtraj as md
+
+traj, mmtop, system, box, positions = water_lib.build_top()
+
+qH = pymc.Uniform("qH", 0.0, 0.5, value=0.417)
+sigma = pymc.Uniform("sigma", 0.3, 0.33, value=0.3151)
+epsilon = pymc.Uniform("epsilon", 0.5, 1.0, value=0.6359)
+theta = pymc.Uniform("theta", 1.7, 2.0, value=1.8242)
+r0 = pymc.Uniform("r0", 0.09, 0.11, value=0.09572)
+
+data = [dict(temperature=281.15, density=999.848), dict(temperature=301.15, density=996.234), dict(temperature=321.15, density=988.927)]
+data = pd.DataFrame(data)
+
+pressure = 1.0 * u.atmospheres
+density_error = 0.1
+
+x = water_lib.build(system, positions, mmtop, 300.0, pressure, qH.value, sigma.value, epsilon.value, r0.value, theta.value)
+
+def calc_density(qH, sigma, epsilon, r0, theta, temperature):
+    print(qH, sigma, epsilon, r0, theta, temperature)
+    x = water_lib.build(system, positions, mmtop, temperature, pressure, qH, sigma, epsilon, r0, theta)
+    t, g, N_eff = pymbar.timeseries.detectEquilibration_fft(x)
+    mu = x[t:].mean()
+    return mu
+
+density_estimators = [
+pymc.Deterministic(lambda qH, sigma, epsilon, r0, theta: calc_density(qH, sigma, epsilon, r0, theta, temperature=t), "Calculates density %d" % i, "density_estimator %d" % i, dict(qH=qH, sigma=sigma, epsilon=epsilon, r0=r0, theta=theta), dtype='float')
+for i, d, t in data.itertuples()
+]
+
+measurements = [
+pymc.Normal("observed_density %d" % i, mu=density_estimators[i], tau=density_error ** -2., value=d, observed=True)
+for i, d, t in data.itertuples()
+]
+
+variables = [qH, sigma, epsilon, theta, r0]
+variables.extend(density_estimators)
+variables.extend(measurements)
+
+model = pymc.Model(variables)
+mcmc = pymc.MCMC(model, db='hdf5', dbname="./water.h5")
+
+mcmc.sample(10000)
+
+
+x = water_lib.build(system, positions, mmtop, temperature, pressure, qH, sigma, epsilon, r0, theta)
---
+++ dbayes/water_lib.py
@@ -0,0 +1,128 @@
+import os
+import tempfile
+import sys
+import pymbar
+import numpy as np
+import pandas as pd
+import simtk.openmm.app as app
+import simtk.openmm as mm
+import simtk.unit as u
+import mdtraj as md
+import repex
+
+ff = app.ForceField("tip3p.xml")
+
+def build_top():
+    box = repex.testsystems.WaterBox()
+    system = box.system
+    positions = box.positions
+    
+    n_atoms = len(box.positions)
+    
+    top = []
+    bonds = []
+    for i in range(n_atoms / 3):
+        j = 3 * i
+        top.append(dict(serial=(j + 1), name="O", element="O", resSeq=(i+1), resName="HOH", chainID=(i+1)))
+        top.append(dict(serial=(j + 2), name="H", element="H", resSeq=(i+1), resName="HOH", chainID=(i+1)))
+        top.append(dict(serial=(j + 3), name="H", element="H", resSeq=(i+1), resName="HOH", chainID=(i+1)))
+        bonds.append([j + 0, j + 1])
+        bonds.append([j + 0, j + 2])
+
+    top = pd.DataFrame(top)
+    bonds = np.array(bonds, dtype='int')
+    top = md.Topology.from_dataframe(top, bonds)
+    
+    xyz = positions / u.nanometers
+    
+    boxes = box.system.getDefaultPeriodicBoxVectors()
+    lengths = boxes[0][0] / u.nanometers * np.ones((1, 3))
+    
+    angles = 90.0 * np.ones((1, 3))
+    traj = md.Trajectory(xyz, top, unitcell_lengths=lengths, unitcell_angles=angles)
+    
+    mmtop = traj.top.to_openmm(traj=traj)
+    
+    return traj, mmtop, system, box, positions
+
+def find_forces(system):
+    for force in system.getForces():
+        if type(force) == mm.NonbondedForce:
+            nonbonded_force = force
+        if type(force) == mm.HarmonicBondForce:
+            bond_force = force
+        if type(force) == mm.HarmonicAngleForce:
+            angle_force = force
+    
+    return bond_force, angle_force, nonbonded_force
+
+def set_constraints(system, r0, theta):
+    r1 = 2 * r0 * np.sin(theta / 2.)
+    n_constraints = system.getNumConstraints()
+    n_water = n_constraints / 3
+
+    for i in range(n_water * 2):
+        a0, a1, d = system.getConstraintParameters(i)
+        system.setConstraintParameters(i, a0, a1, r0 * u.nanometers)
+
+    for i in range(n_water * 2, n_water * 3):
+        a0, a1, d = system.getConstraintParameters(i)
+        system.setConstraintParameters(i, a0, a1, r1 * u.nanometers)
+
+def set_nonbonded(f_nonbonded, qH, sigma, epsilon):
+    qO = -2.0 * qH
+    for k in range(f_nonbonded.getNumParticles()):
+        if k % 3 == 0:
+            f_nonbonded.setParticleParameters(k, qO * u.elementary_charge, sigma * u.nanometer, epsilon * u.kilojoule_per_mole)
+        else:
+            f_nonbonded.setParticleParameters(k, qH * u.elementary_charge, 1.0 * u.nanometer, 0.0 * u.kilojoule_per_mole)
+
+def set_parms(system, qH, sigma, epsilon, r0, theta):
+    print("\nqH=%f, sigma=%f, epsilon=%f r0=%f, theta=%f\n" % (qH, sigma, epsilon, r0, theta))
+    f_bond, f_angle, f_nonbonded = find_forces(system)
+    
+    set_constraints(system, r0, theta)
+    set_nonbonded(f_nonbonded, qH, sigma, epsilon)
+
+
+def build(system, positions, mmtop, temperature, pressure, qH, sigma, epsilon, r0, theta, stderr_tolerance=0.05, n_steps=250000, nonbondedCutoff=1.1 * u.nanometer, output_frequency=250, print_frequency=None):
+    
+    if print_frequency is None:
+        print_frequency = int(n_steps / 3.)
+    
+    set_parms(system, qH, sigma, epsilon, r0, theta)
+
+    friction = 1.0 / u.picoseconds
+    timestep = 3.0 * u.femtoseconds
+    barostat_frequency = 25
+
+    path = tempfile.mkdtemp()
+    csv_filename = os.path.join(path, "density.csv")
+
+    integrator = mm.LangevinIntegrator(temperature, friction, timestep)
+    system.addForce(mm.MonteCarloBarostat(pressure, temperature, barostat_frequency))
+
+    simulation = app.Simulation(mmtop, system, integrator)
+
+    simulation.reporters.append(app.StateDataReporter(sys.stdout, print_frequency, step=True, density=True))
+    simulation.reporters.append(app.StateDataReporter(csv_filename, output_frequency, density=True))
+    simulation.context.setPositions(positions)
+
+    print("minimizing")
+    simulation.minimizeEnergy()
+    simulation.context.setVelocitiesToTemperature(temperature)
+    print("done minimizing")
+
+    converged = False
+    while not converged:
+        simulation.step(n_steps)
+        d = pd.read_csv(csv_filename, names=["Density"], skiprows=1)
+        density_ts = np.array(d.Density)
+        [t0, g, Neff] = pymbar.timeseries.detectEquilibration_fft(density_ts)
+        density_ts = density_ts[t0:]
+        density_mean_stderr = density_ts.std() / np.sqrt(Neff)
+        if density_mean_stderr < stderr_tolerance:
+            converged = True
+    print("temperature, density mean, stderr = %f, %f, %f" % (temperature / u.kelvin, density_ts.mean(), density_mean_stderr))
+    return d.Density.values
+
