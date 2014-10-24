afa2334a3e0e917165d7b1c6712020d1126e437c
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-10-24 18:54:44 -0400

    Update

--- dbayes/test_dipoles_grid.py
+++ dbayes/test_dipoles_grid.py
@@ -10,7 +10,7 @@ import mdtraj as md
 
 
 out_dir = "./symmetric/"
-q0 = pymc.Uniform("q0", 0.0, 0.5)
+q0 = pymc.Uniform("q0", 0.0, 1.0, value=1.0, observed=True)
 sigma0 = pymc.Uniform("sigma0", 0.08, 0.4)
 epsilon0 = pymc.Uniform("epsilon0", 0.2, 2.0)
 #sigma1 = pymc.Uniform("sigma0", 0.08, 0.4)
@@ -21,12 +21,12 @@ r0 = pymc.Uniform("r0", 0.05, 0.25, value=0.2, observed=True)
 
 model = pymc.Model([q0, sigma0, epsilon0, sigma1, epsilon1, r0])
 
-temperature = 300 * u.kelvin
+temperature = 280 * u.kelvin
 pressure = 1.0 * u.atmospheres
 
 data = []
 
-for k in range(100):
+for k in range(250):
     model.draw_from_prior()
     dipole = dipoles.Dipole(1000, q0=q0.value, sigma0=sigma0.value, epsilon0=epsilon0.value, sigma1=sigma1.value, epsilon1=epsilon1.value, r0=r0.value)
     print(dipole)
--- dbayes/test_pymc_water.py
+++ dbayes/test_pymc_water.py
@@ -10,10 +10,12 @@ import mdtraj as md
 
 traj, mmtop, system, box, positions = water_lib.build_top()
 
-qH = pymc.Uniform("qH", 0.40, 0.5, value=0.417, observed=True)
-sigma = pymc.Uniform("sigma", 0.31, 0.325, value=0.314)  # 0.31507524065751241
+n_steps = 200000
+qH = pymc.Uniform("qH", 0.40, 0.5, value=0.417)
+sigma = pymc.Uniform("sigma", 0.31, 0.325, value=0.3147)  # 0.31507524065751241
 epsilon = pymc.Uniform("epsilon", 0.5, 1.0, value=0.635968, observed=True)
-sigmaH = pymc.Uniform("sigmaH", 0.06, 0.12, value=0.1)
+#sigmaH = pymc.Uniform("sigmaH", 0.06, 0.12, value=0.1004)
+sigmaH = pymc.Uniform("sigmaH", 0.06, 0.13, value=0.089)
 epsilonH = pymc.Uniform("epsilonH", 0.03, 0.15, value=0.06, observed=True)
 theta = pymc.Uniform("theta", 1.7, 2.0, value=1.82421813418, observed=True)
 r0 = pymc.Uniform("r0", 0.09, 0.11, value=0.09572, observed=True)
@@ -22,10 +24,11 @@ data = [dict(temperature=281.15 * u.kelvin, density=0.999848), dict(temperature=
 data = pd.DataFrame(data)
 
 pressure = 1.0 * u.atmospheres
-density_error = 0.001
+#density_error = 0.001
+density_error = 0.00075
 
 def calc_density(qH, sigma, epsilon, sigmaH, epsilonH, r0, theta, temperature):
-    x = water_lib.build(system, positions, mmtop, temperature * u.kelvin, pressure, qH, sigma, epsilon, sigmaH, epsilonH, r0, theta, stderr_tolerance=density_error)
+    x = water_lib.build(system, positions, mmtop, temperature * u.kelvin, pressure, qH, sigma, epsilon, sigmaH, epsilonH, r0, theta, stderr_tolerance=density_error, n_steps=n_steps)
     t, g, N_eff = pymbar.timeseries.detectEquilibration_fft(x)
     mu = x[t:].mean()
     print(temperature, mu)
@@ -44,12 +47,25 @@ pymc.Normal("observed_density_%d" % i, mu=density_estimators[i], tau=density_err
 for i in data.index
 ]
 
+pred = np.array([d.value for d in density_estimators])
+expt = data.density.values
+z = (pred - expt) / density_error
+z
+sum(z ** 2)
+sum([q.logp for q in measurements])  # This is -1 * chi squared, up to an additive constant
+
+
+
 variables = [qH, sigma, epsilon, sigmaH, epsilonH, theta, r0]
 variables.extend(temperatures)
 variables.extend(density_estimators)
 variables.extend(measurements)
 
-mcmc = pymc.MCMC(variables, db='hdf5', dbname="./water.h5")
+#map = pymc.MAP(variables)
+#map.fit()
+
+#mcmc = pymc.MCMC(variables, db='hdf5', dbname="./water.h5", verbose=3)
+mcmc = pymc.MCMC(variables, verbose=3)
 
 mcmc.use_step_method(pymc.Metropolis, qH, proposal_sd=0.001, proposal_distribution='Normal')
 mcmc.use_step_method(pymc.Metropolis, sigma, proposal_sd=0.0005, proposal_distribution='Normal')
