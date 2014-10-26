b45e0f76c058a8a3748bf5943c8bd2baca738589
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-10-26 13:42:52 -0400

    Update

--- dbayes/test_dipoles_simple.py
+++ dbayes/test_dipoles_simple.py
@@ -24,12 +24,11 @@ model = pymc.Model([q0, sigma0, epsilon0, sigma1, epsilon1, r0])
 temperatures = [280 * u.kelvin, 300 * u.kelvin, 320 * u.kelvin]
 pressure = 1.0 * u.atmospheres
 
-for k in range(2):
-    model.draw_from_prior()
-    for temperature in temperatures:
-        dipole = dipoles.Dipole(1000, q0=q0.value, sigma0=sigma0.value, epsilon0=epsilon0.value, sigma1=sigma1.value, epsilon1=epsilon1.value, r0=r0.value)
-        print(dipole)
-        try:
-            values, mu, sigma = dipoles.simulate_density(dipole, traj, temperature, pressure, out_dir)
-        except Exception as e:
-            print(e)
+model.draw_from_prior()
+for temperature in temperatures:
+    dipole = dipoles.Dipole(1000, q0=q0.value, sigma0=sigma0.value, epsilon0=epsilon0.value, sigma1=sigma1.value, epsilon1=epsilon1.value, r0=r0.value)
+    print(dipole)
+    try:
+        values, mu, sigma = dipoles.simulate_density(dipole, traj, temperature, pressure, out_dir)
+    except Exception as e:
+        print(e)
